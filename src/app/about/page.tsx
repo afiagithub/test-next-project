cc06c1776611dd65f029e681c07432de7f920792
Afia Rahman <rahmanmou26@gmail.com>
2024-09-09 21:30:22 +0600

    added 2 static page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const aboutCompo = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-center mt-10'>This is about page</h1>
+    </div>
+  )
+}
+
+export default aboutCompo
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contactinfo = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-center mt-10'>This is contact page</h1>
+    </div>
+  )
+}
+
+export default contactinfo
